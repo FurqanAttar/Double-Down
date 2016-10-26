4bc238700e0080c7c4cf7ec26fe4c023af8b1732
Furqan Attar <SumiFurqan@10-107-215-120.vlan895.client.wireless.uic.edu>
2016-10-26 09:47:59 -0500

    Initial Commit

---
+++ Proj4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proj4
+//
+//  Created by Furqan Attar on 10/26/16.
+//  Copyright © 2016 Furqan Attar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
